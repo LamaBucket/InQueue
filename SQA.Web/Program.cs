dc9fb6e569dfe8e82e61a58c5345c828c6b04d37
gleb <gleb.bny@gmail.com>
2024-03-16 22:12:06 +0300

    FIX: Fix error with hub not mapping users to clients

--- SQA.Web/Program.cs
+++ SQA.Web/Program.cs
@@ -8,67 +8,84 @@ using SQA.EntityFramework.Services;
 using SQA.Web;
 using SQA.Web.Middlewares;
 using SQA.Web.Controllers;
+using Microsoft.AspNetCore.SignalR;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-builder.Services.AddLogging(builder =>
+internal class Program
 {
-    builder.AddFile();
-});
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddMvc();
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.Services.AddSignalR(x => x.EnableDetailedErrors = true);
+        builder.Services.AddLogging(builder =>
+        {
+            builder.AddFile();
+        });
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
-{
-    options.Cookie.Name = "Authentication";
-    options.AccessDeniedPath = "/Authenticate";
-});
-builder.Services.AddAuthorization();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        builder.Services.AddMvc();
 
-builder.Services.AddTransient<SQADbContextFactory>();
+        builder.Services.AddSingleton<IUserIdProvider, HubUserProvider>();
 
-builder.Services.AddScoped<IUserDataService, UserDataService>();
-builder.Services.AddScoped<IQueueDataService, QueueDataService>();
-builder.Services.AddScoped<IUserRoleDataService, UserRoleDataService>();
+        builder.Services.AddSignalR(x => x.EnableDetailedErrors = true);
 
-builder.Services.AddScoped<IStringHasher, PasswordHasher>();
-builder.Services.AddScoped<IUserBuilder, UserBuilder>();
-builder.Services.AddScoped<IUserPasswordProvider, UserPasswordProvider>();
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+        {
+            options.Cookie.Name = "Authentication";
+            options.AccessDeniedPath = "/Authenticate";
+        });
+        builder.Services.AddAuthorization();
 
-builder.Services.AddScoped<IQueueBuilder, QueueBuilder>();
+        builder.Services.AddTransient<SQADbContextFactory>();
+
+        builder.Services.AddScoped<IUserDataService, UserDataService>();
+        builder.Services.AddScoped<IQueueDataService, QueueDataService>();
+        builder.Services.AddScoped<IUserRoleDataService, UserRoleDataService>();
+
+        builder.Services.AddScoped<IStringHasher, PasswordHasher>();
+        builder.Services.AddScoped<IUserBuilder, UserBuilder>();
+        builder.Services.AddScoped<IUserPasswordProvider, UserPasswordProvider>();
+
+        builder.Services.AddScoped<IQueueBuilder, QueueBuilder>();
 
 
-var app = builder.Build();
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+        app.UseMiddleware<LoggerMiddleware>();
+
+        app.MapHub<QueueHub>("/queue");
+
+        app.MapControllerRoute(
+            name: "Default",
+            "{controller}",
+            new { controller = "WebApp" });
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-app.UseMiddleware<LoggerMiddleware>();
-
-app.MapHub<QueueHub>("/queue");
-
-app.MapControllerRoute(
-    name: "Default",
-    "{controller}",
-    new { controller = "WebApp"});
-
-app.Run();
+public class HubUserProvider : IUserIdProvider
+{
+    public string? GetUserId(HubConnectionContext connection)
+    {
+        return connection.User.Identity?.Name;
+    }
+}
